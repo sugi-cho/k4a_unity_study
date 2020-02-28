7eebaa01457c198b5db40bf2b611106cacb4d179
sugino <git@sugi.cc>
2020-02-28 17:42:11 +0900

    AzureKinect SDK を突っ込んだ。APIを.Net 4.x に

---
+++ Assets/k4a_unity/Scripts/OpenKinect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Microsoft.Azure.Kinect.Sensor;
+
+//https://tks-yoshinaga.hatenablog.com/entry/azurekinect-5
+public class OpenKinect : MonoBehaviour
+{
+
+    Device kinect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        kinect = Device.Open(0);
+        kinect.StartCameras(new DeviceConfiguration
+        {
+            ColorFormat = ImageFormat.ColorBGRA32,
+            ColorResolution = ColorResolution.R720p,
+            DepthMode = DepthMode.NFOV_2x2Binned,
+            SynchronizedImagesOnly = true,
+            CameraFPS = FPS.FPS30,
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        kinect.StopCameras();
+    }
+}
